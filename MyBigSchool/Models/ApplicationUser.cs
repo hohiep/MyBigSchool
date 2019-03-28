294ebe16fc7347d22f1810c3b7ea29afc45f092c
Administrator <Administrator@PC025>
2019-03-28 11:19:32 +0700

    67

--- MyBigSchool/Models/ApplicationUser.cs
+++ MyBigSchool/Models/ApplicationUser.cs
@@ -20,6 +20,8 @@ namespace MyBigSchool.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
